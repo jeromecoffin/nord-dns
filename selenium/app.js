afcb6b560a094bd386c3e847e4d0143aabb32f61
Alexandre Parès <IAmFrench@users.noreply.github.com>
2021-03-01 23:18:15 +0100

    Add automated test for firefox using nord dns as DoH #48

---
+++ selenium/app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const {Builder} = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+let fs = require("fs");
+
+
+const options = new firefox.Options();
+
+options.setPreference("network.trr.custom_uri", "https://ndns.cf:8443/l/1Hosts");
+options.setPreference("network.trr.mode", 3);
+options.setPreference("network.trr.uri", "https://ndns.cf:8443/l/1Hosts");
+options.setPreference("network.trr.useGET", true);
+
+
+const pages = [
+	"https://www.20minutes.fr/",
+	"https://www.20minutes.fr/sante/2986387-20210226-coronavirus-quoi-pass-sanitaire-evoque-emmanuel-macron",
+	"https://www.20minutes.fr/monde/2985915-20210225-etats-unis-procureur-manhattan-obtenu-millions-pages-declarations-impots-donald-trump",
+	"https://www.nytimes.com/",
+	"https://www.nytimes.com/2021/02/25/technology/clubhouse-audio-app-experience.html",
+	"https://www.nytimes.com/2021/02/25/technology/net-neutrality-explained.html",
+	"https://www.aljazeera.com/",
+	"https://www.aljazeera.com/news/2021/2/26/unconstitutional-us-airstrikes-on-syria-draw-strong-criticism",
+	"https://www.aljazeera.com/opinions/2021/2/24/a-call-for-global-vaccine-equity",
+	"https://www.europe1.fr/",
+	"https://www.europe1.fr/societe/face-aux-critiques-la-mairie-de-paris-retropedale-sur-un-reconfinement-total-4027854",
+	"https://www.europe1.fr/politique/un-deferlement-dinsultes-un-maire-de-la-region-lyonnaise-a-nouveau-menace-4027685",
+	"https://www.purepeople.com/",
+	"https://www.purepeople.com/article/golden-globes-brad-pitt-renee-zellweger-joaquin-phoenix-le-palmares_a368192/1",
+	"https://www.purepeople.com/article/festival-de-cannes-2020-une-petite-edition-hors-normes-programmee_a405602/1"
+];
+
+(async function myFunction() {
+	let driver = await new Builder()
+		.forBrowser("firefox")
+		.setFirefoxOptions(options)
+		.usingServer("http://172.17.0.1:4444/wd/hub")
+		.build();
+
+	for (let index = 0; index < pages.length; index++) {
+		const pageUri = pages[index];
+		await driver.get(pageUri);
+		let encodedString = await driver.takeScreenshot();
+		let now = (new Date()).toISOString().replace("T", " ").replaceAll(":","-").split(".")[0];
+		await fs.writeFileSync(`screenshots/${now}_${index}.png`, encodedString, "base64");
+	}
+
+	await driver.quit();
+})();
